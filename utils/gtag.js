44361095179d262fc79efb4f33aa6694b7870379
Mert Erbil <mertolerbil@gmail.com>
2020-06-24 15:00:57 +0300

    Update gtag.js

--- utils/gtag.js
+++ utils/gtag.js
@@ -1,2 +1,2 @@
-export const GA_TRACKING_ID = 'UA-157718657-1'
+export const GA_TRACKING_ID = 'UA-170655545-1'
 export const pageView = url => window.gtag('config', GA_TRACKING_ID, { page_path: url })
